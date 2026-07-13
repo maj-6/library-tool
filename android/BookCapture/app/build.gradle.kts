8f232023877c6579ac51b4863cda9d06badb98f0
Andrew James Miller <amiller4096@gmail.com>
2026-07-12 23:09:25 -0700

    Cut v0.7.0-alpha.6

--- android/BookCapture/app/build.gradle.kts
+++ android/BookCapture/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "org.whl.bookcapture"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "0.5.0-alpha.5"
+        versionCode = 12
+        versionName = "0.5.0-alpha.6"
         buildConfigField("String", "SUPABASE_URL", "\"${env("WHL_SUPABASE_URL")}\"")
         buildConfigField("String", "SUPABASE_ANON_KEY", "\"${env("WHL_SUPABASE_ANON_KEY")}\"")
     }
