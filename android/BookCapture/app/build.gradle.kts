369a712c341fd8a68ec07a81ef7a45ec621d2bc5
Andrew James Miller <amiller4096@gmail.com>
2026-08-09 14:36:46 -0700

    Prepare v0.9.0-alpha.16

--- android/BookCapture/app/build.gradle.kts
+++ android/BookCapture/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "org.whl.bookcapture"
         minSdk = 26
         targetSdk = 34
-        versionCode = 45
-        versionName = "0.5.2-alpha.14"
+        versionCode = 46
+        versionName = "0.5.2-alpha.15"
         buildConfigField("String", "SUPABASE_URL", "\"${env("WHL_SUPABASE_URL")}\"")
         buildConfigField("String", "SUPABASE_ANON_KEY", "\"${env("WHL_SUPABASE_ANON_KEY")}\"")
     }
